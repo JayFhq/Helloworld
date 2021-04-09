092da23ccca7fdd04f5650bfb95fa294fbe8fd8f
JayFHQ <2645479260@qq.com>
2021-04-09 11:05:23 +0800

    version 1.0

--- src/hellow/Helloworld.java
+++ src/hellow/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("This is my first git project");
         System.out.println("Update this primary project");
+        System.out.println("");
     }
 }
